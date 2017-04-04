69ec015c2851454f7e55a06899d781a8079f225e
Alexia E. Smith <washuu@gmail.com>
2017-04-04 02:34:01 -0500

    Not Broken.

--- CAN-CCD_bus.ino
+++ CAN-CCD_bus.ino
@@ -18,12 +18,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define XJ_PLATFORM true
 
 #include "include/CCD/CCD.cpp"
-#include "include/CAN/HaltechCAN.cpp"
+//#include "include/CAN/HaltechCAN.cpp"
 
 const boolean debug = true;
 
 const int led = LED_BUILTIN;
-HaltechCAN canBus;
+//HaltechCAN canBus;
 CCD ccdBus(Serial1);
 
 float lastMPH = 0;
